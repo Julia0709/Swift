9aa0ca64f8eac145f880f4dd50df82cd805c9583
Julia <berrylily.0709@gmail.com>
2017-09-13 18:50:06 -0700

    Implement generateNewNumber func

--- CUI2048Plus/CUI2048Plus/main.swift
+++ CUI2048Plus/CUI2048Plus/main.swift
@@ -8,32 +8,51 @@
 
 import Foundation
 
-print("Welcome to 2048+!")
-
 var matrix: [[Int]] = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
-for i in matrix {
-        print(" \(i) ")
+func generateNewNumber() {
+    var emptyTiles = [[Int]]()
+    for (x, row) in matrix.enumerated() {
+        for (y, n) in row.enumerated() {
+            if (n == 0) {
+                emptyTiles.append([x, y])
+            }
+        }
+    }
+    let randomIndex = Int(arc4random_uniform(UInt32(emptyTiles.count)))
+    let t = emptyTiles[randomIndex]
+    matrix[t[0]][t[1]] = 2
+    
 }
 
-print("i(↑)  j(←)  k(↓)  l(→) | p(pose)")
-var move = readLine()
+print("Welcome to 2048+!")
 
-switch(move) {
-    case "i"?:
-        // shiftUp()
+generateNewNumber()
+
+while (true) {
+
+    generateNewNumber()
+    print(matrix)
+
+    print("i(↑)  j(←)  k(↓)  l(→) | p(pose)")
+    var move: String = readLine()!
+
+    switch(move) {
+        case "i":
+            // shiftUp()
+            break
+        case "j":
+            // shiftLeft()
+            break
+        case "k":
+            // shiftDown()
         break
-    case "j"?:
-        // shiftLeft()
-        break
-    case "k"?:
-        // shiftDown()
-    break
-    case "l"?:
-        // shiftRight()
-        break
-    case "p"?:
-        break
-    default:
-        print("Enter again:\ni(↑)  j(←)  k(↓)  l(→) | p(pose)")
+        case "l":
+            // shiftRight()
+            break
+        case "p":
+            break
+        default:
+            print("Enter again:\ni(↑)  j(←)  k(↓)  l(→) | p(pose)")
+    }
 }
