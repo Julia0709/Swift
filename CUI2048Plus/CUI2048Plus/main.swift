5bf851be0e4f9e12b6b1967f760f6902acc61a26
Julia <berrylily.0709@gmail.com>
2017-09-13 17:48:35 -0700

    Print matrix

--- CUI2048Plus/CUI2048Plus/main.swift
+++ CUI2048Plus/CUI2048Plus/main.swift
@@ -10,3 +10,30 @@ import Foundation
 
 print("Welcome to 2048+!")
 
+var matrix: [[Int]] = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+for i in matrix {
+        print(" \(i) ")
+}
+
+print("i(↑)  j(←)  k(↓)  l(→) | p(pose)")
+var move = readLine()
+
+switch(move) {
+    case "i"?:
+        // shiftUp()
+        break
+    case "j"?:
+        // shiftLeft()
+        break
+    case "k"?:
+        // shiftDown()
+    break
+    case "l"?:
+        // shiftRight()
+        break
+    case "p"?:
+        break
+    default:
+        print("Enter again:\ni(↑)  j(←)  k(↓)  l(→) | p(pose)")
+}
