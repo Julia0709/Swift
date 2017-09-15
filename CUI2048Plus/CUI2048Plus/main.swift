34852ed4d961a8ce5d56a918d3528a0b53d201e6
Julia <berrylily.0709@gmail.com>
2017-09-14 17:52:30 -0700

    Implement quit functionality

--- CUI2048Plus/CUI2048Plus/main.swift
+++ CUI2048Plus/CUI2048Plus/main.swift
@@ -35,7 +35,7 @@ func selectMode(mode: inout String) -> Int {
         goal = 8192
         break
     default:
-       print("Please type 1, 2, 3 or 4: ")
+       print("Please enter 1, 2, 3 or 4.")
         return goal
     }
     print(mode + " MODE: GAME START!");
@@ -81,110 +81,10 @@ func displayTiles() {
     print("+---------------------------+")
 }
 
-// Rotate matrix clockwise
-func rotate(matrix: inout [[Int]], times: inout Int) -> [[Int]] {
-    while (times > 0) {
-        var rotatedMatrix = Array(repeating: Array(repeating: 0, count: length), count: length)
-        for y in 0..<matrix.count {
-            let l = matrix[y].count
-            for x in 0..<l {
-                rotatedMatrix[y][x] = matrix[l - x - 1][y]
-            }
-        }
-        matrix = rotatedMatrix
-        times -= 1
-    }
-    return matrix
-}
-
-// Shift left
-func shiftLeft(matrix: inout [[Int]]) -> [[Int]] {
-    for y in 0..<matrix.count {
-        let row = matrix[y]
-        for x1 in 0..<row.count-1 {
-            let n1 = matrix[y][x1]
-            if (n1 == 0) {
-                continue
-            }
-            for x2 in x1+1..<row.count {
-                let n2 = matrix[y][x2]
-                if (n2 == 0) {
-                    continue
-                }
-                if (n1 != n2) {
-                    break
-                }
-                matrix[y][x1] = n1 + n2
-                matrix[y][x2] = 0
-                break
-            }
-        }
-
-        var positions = [Int]()
-        for x in 0..<row.count {
-            let n = row[x]
-            if (n == 0) {
-                positions.append(x)
-                continue
-            }
-            if (positions.count > 0) {
-                matrix[y][positions[0]] = n
-                matrix[y][x] = 0
-                positions.remove(at: 0)
-                positions.append(x)
-            }
-        }
-    }
-    return matrix
-}
-
-// Shift up
-func shiftUp() {
-    // Rotate 270˚ clockwise
-    time = 3
-    rotate(matrix: &originalMatrix, times: &time)
-
-    // Shift tiles
-    shiftLeft(matrix: &originalMatrix)
-    
-    // Rotate 90˚ clockwise
-    time = 1
-    rotate(matrix: &originalMatrix, times: &time)
-}
-
-// Shift down
-func shiftDown() {
-    // Rotate 90˚ clockwise
-    time = 1
-    rotate(matrix: &originalMatrix, times: &time)
-
-
-    // Shift tiles
-    shiftLeft(matrix: &originalMatrix)
-    
-    // Rotate 270˚ clockwise
-    time = 3
-    rotate(matrix: &originalMatrix, times: &time)
-}
-
-// Shift right
-func shiftRight() {
-    // Rotate 180˚ clockwise
-    time = 2
-    rotate(matrix: &originalMatrix, times: &time)
-    
-    
-    // Shift tiles
-    shiftLeft(matrix: &originalMatrix)
-    
-    // Rotate 180˚ clockwise
-    time = 2
-    rotate(matrix: &originalMatrix, times: &time)
-}
 
 print("\n=============================")
 print("      Welcome to 2048+       ")
-print("============================")
+print("=============================")
 
 var goal:Int = 0
 while (goal == 0) {
@@ -195,36 +95,43 @@ while (goal == 0) {
 
 generateNewNumber()
 
-while (true) {
+var onGame: Bool = true
+
+while (onGame) {
 
     generateNewNumber()
 
     displayTiles()
 
-    print("i: UP(↑)  j: LEFT(←)  k: DOWN(↓)  l: RIGHT(→) | r(RESET)")
+    print("i: UP(↑)  j: LEFT(←)  k: DOWN(↓)  l: RIGHT(→) | q: (QUIT)")
     var move: String = readLine()!
+    
+    var shift = ManageShift()
 
     switch(move) {
         case "i":
             print("UP(↑)")
-            shiftUp()
+            shift.shiftUp()
             break
         case "j":
             print("LEFT(←)")
-            shiftLeft(matrix: &originalMatrix)
+            shift.shiftLeft(matrix: &originalMatrix)
             break
         case "k":
             print("DOWN(↓)")
-            shiftDown()
+            shift.shiftDown()
             break
         case "l":
             print("RIGHT(→)")
-            shiftRight()
+            shift.shiftRight()
             break
-        case "r":
-            print("RESET")
+        case "q":
+            print("QUIT")
+            onGame = false
             break
         default:
-            print("Enter again:\ni(↑)  j(←)  k(↓)  l(→) | p(pose)")
+            print("Oops! Please enter again.")
     }
 }
+
+print("GAME OVER")
